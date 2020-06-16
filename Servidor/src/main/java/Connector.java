6c0c651a2f0b4f013011ebe8c86532e6c56af9c5
Bianca Pilegi <46908195+bpilegi98@users.noreply.github.com>
2020-06-16 15:56:08 -0300

    Base
    Base del trabajo, tenemos de momento servidor y cliente por separado, para que funcione correctamente, se debe ejecutar el servidor y luego el cliente.

---
+++ Servidor/src/main/java/Connector.java
@@ -0,0 +1,49 @@
+import java.net.*;
+import java.io.*;
+
+public class Connector {
+
+    ServerSocket server;
+    Socket socket;
+    //Socket cliente;
+    //String ip = "127.0.0.1";
+    int port = 9000;
+    DataOutputStream salida;
+    BufferedReader entrada;
+
+
+
+    public void iniciar()
+    {
+        try {
+
+            server = new ServerSocket(port);
+            socket = new Socket();
+            socket = server.accept();
+
+            entrada = new BufferedReader(new InputStreamReader(socket.getInputStream()));
+            String mensaje = entrada.readLine();
+            System.out.println(mensaje);
+            salida = new DataOutputStream(socket.getOutputStream());
+            salida.writeUTF("Adios mundo");
+            socket.close();
+
+            /*
+            cliente = new Socket(ip, port);
+            entrada = new BufferedReader(new InputStreamReader(cliente.getInputStream()));
+            teclado = new BufferedReader(new InputStreamReader(System.in));
+            String tec = teclado.readLine();
+            salida = new PrintStream(cliente.getOutputStream());
+            salida.println(tec);
+            String msg = entrada.readLine();
+            System.out.println(msg);
+
+            entrada.close();
+            salida.close();
+            teclado.close();
+            cliente.close();
+             */
+
+        }catch(Exception e){};
+    }
+}
---
+++ Servidor/src/main/java/Main.java
@@ -0,0 +1,9 @@
+public class Main {
+    public static void main(String[] args) {
+
+        Connector c = new Connector();
+
+        c.iniciar();
+
+    }
+}
